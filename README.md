6ccafa12f157f90926ba8c24d88eab911a600fac
Charlie Xu <u7076870@anu.edu.au>
2020-09-08 00:15:24 +0800

    Theme Set.

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Sample group project repo for COMP2100/6442 (see [Wiki](https://gitlab.cecs.anu.edu.au/u1064702/comp2100_6442_s2_2020_group_project/-/wikis/home))
+Group project repo for COMP2100/6442 by Fantasty Inc. (see [Wiki](https://gitlab.cecs.anu.edu.au/u7076870/comp2100_6442_s2_2020_group_project/-/wikis/home))
---
+++ app/src/main/java/com/example/wtfood/SplashScreen.java
@@ -0,0 +1,23 @@
+package com.example.wtfood;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
