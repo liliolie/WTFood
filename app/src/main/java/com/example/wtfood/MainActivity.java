bb8fa4a2326d5ca1a8fc8ace4997d7daf4584139
Junliang Liu <u7096757@anu.edu.au>
2020-10-11 12:28:56 +0800

    add RBTree inserting and file reading in MainActivity

--- app/src/main/java/com/example/wtfood/MainActivity.java
+++ app/src/main/java/com/example/wtfood/MainActivity.java
@@ -8,7 +8,17 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.example.wtfood.fileprocess.FileProcess;
+import com.example.wtfood.fileprocess.Restaurant;
+import com.example.wtfood.rbtree.RBTree;
+
+import java.io.IOException;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RBTree priceTree;
+    private RBTree raringTree;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +27,19 @@ public class MainActivity extends AppCompatActivity {
 //        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 //                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        priceTree = new RBTree("price");
+        raringTree = new RBTree("rating");
+
+        try {
+            List<Restaurant> restaurants = new FileProcess().fileRead(getAssets().open("list.json"));
+            for (Restaurant r : restaurants) {
+                priceTree.insert(r);
+                raringTree.insert(r);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void logoButton(View view){
