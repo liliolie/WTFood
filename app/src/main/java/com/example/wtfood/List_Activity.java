135a6bab7d0a408a9a5f0b8fbced535d62302297
Yen Kuo <u7075575@anu.edu.au>
2020-10-13 03:10:15 +1100

    Result Page move to main.

--- app/src/main/java/com/example/wtfood/List_Activity.java
+++ app/src/main/java/com/example/wtfood/List_Activity.java
@@ -12,11 +12,12 @@ import com.example.wtfood.fileprocess.Location;
 import com.example.wtfood.fileprocess.Restaurant;
 import com.example.wtfood.fileprocess.Type;
 
+import java.sql.SQLOutput;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 
 public class List_Activity extends AppCompatActivity {
-    public ListView result;
+    ListView result;
 
 
     @Override
@@ -25,54 +26,14 @@ public class List_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_list_);
 
         result = (ListView) findViewById(R.id.result_lv);
-        ArrayList<String> arr= new ArrayList<>();
-        ArrayList<Restaurant> AR= new ArrayList<>();
-        AR.add(createRestaurant("Hero burger"));
-        AR.add(createRestaurant("KFC"));
-        AR.add(createRestaurant("Subway"));
-        AR.add(createRestaurant("Tacobell"));
-        AR.add(createRestaurant("BurgerKing"));
-        AR.add(createRestaurant("Raku"));
+        ArrayList<String> R = getIntent().getStringArrayListExtra("R");
 
-        for (int i = 0; i < AR.size(); i ++){
-            arr.add(display(AR.get(i)));
-        }
 
-        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_list_item_1, arr);
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_list_item_1, R);
         result.setAdapter(aa);
-    }
-
-    public String display(Restaurant r){
-        String Address = r.getAddress();
-        String name = r.getName();
-        int rating = r.getRating();
-        int price = r.getPrice();
-        String phone = r.getPhone();
-        String delivery = "";
-        String type = "";
-        if(r.isDeliveryService()){
-            delivery = "Deliverable.";
-        }
-        else {
-            delivery = "Unable to for delivery.";
-        }
-
-        if(r.getType().equals(Type.chinesefood))
-        {
-            type = "Chinese Food!";
-        }
-        if(r.getType().equals(Type.fastfood))
-        {
-            type = "Fast Food!";
-        }
-
-        return name + " \n " + Address + " | " + phone + " \n " + "Rating: " + Integer.toString(rating) + " \n " + "Price: "+
-                Integer.toString(price) + " \n " + delivery + " \n " + "FoodType: " + type;
 
     }
 
-    private Restaurant createRestaurant(String name){
-       Restaurant r = new Restaurant(name);
-       return r;
-    }
+
+
 }
--- app/src/main/java/com/example/wtfood/MainActivity.java
+++ app/src/main/java/com/example/wtfood/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +16,7 @@ import android.widget.ListView;
 
 import com.example.wtfood.fileprocess.FileProcess;
 import com.example.wtfood.fileprocess.Restaurant;
+import com.example.wtfood.fileprocess.Type;
 import com.example.wtfood.rbtree.Node;
 import com.example.wtfood.rbtree.RBTree;
 
@@ -26,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
 
     private RBTree priceTree;
     private RBTree raringTree;
+    private ArrayList<String> restaurant_info;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,23 +53,72 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
+        // Here can add our data in the array list.
+        restaurant_info = new ArrayList<>();
+        ArrayList<Restaurant> restaurants = new ArrayList<>();
+        restaurants.add(createRestaurant("Hero burger"));
+        restaurants.add(createRestaurant("KFC"));
+        restaurants.add(createRestaurant("Subway"));
+        restaurants.add(createRestaurant("Tacobell"));
+        restaurants.add(createRestaurant("BurgerKing"));
+        restaurants.add(createRestaurant("Raku"));
+
+        for (int i = 0; i < restaurants.size(); i++) {
+            restaurant_info.add(display(restaurants.get(i)));
+        }
+        ImageButton go = (ImageButton) findViewById(R.id.goButton);
+        go.setOnClickListener(l1);
     }
 
 
-
-    public void logoButton(View view){
+    public void logoButton(View view) {
         Intent intent = new Intent(this, InfoPage.class);
         startActivity(intent);
     }
 
-    public void tutorialButton(View view){
+    public void tutorialButton(View view) {
         Intent intent = new Intent(this, TutorialPage.class);
         startActivity(intent);
     }
-    public void goButton(View view){
-        Intent intent = new Intent(this, List_Activity.class);
-        startActivity(intent);
+
+    private View.OnClickListener l1 = new View.OnClickListener() {
+        public void onClick(View v) {
+            // Passing the array to List_Activity.
+            Intent intent = new Intent(getApplicationContext(), List_Activity.class);
+            intent.putExtra("R", restaurant_info);
+            startActivity(intent);
+        }
+    };
+
+
+    public String display(Restaurant r) {
+        String Address = r.getAddress();
+        String name = r.getName();
+        int rating = r.getRating();
+        int price = r.getPrice();
+        String phone = r.getPhone();
+        String delivery = "";
+        String type = "";
+        if (r.isDeliveryService()) {
+            delivery = "Deliverable.";
+        } else {
+            delivery = "Unable to for delivery.";
+        }
+
+        if (r.getType().equals(Type.chinesefood)) {
+            type = "Chinese Food!";
+        }
+        if (r.getType().equals(Type.fastfood)) {
+            type = "Fast Food!";
+        }
+
+        return name + " \n " + Address + " | " + phone + " \n " + "Rating: " + Integer.toString(rating) + " \n " + "Price: " +
+                Integer.toString(price) + " \n " + delivery + " \n " + "FoodType: " + type;
+
     }
 
-
+    private Restaurant createRestaurant(String name) {
+        Restaurant r = new Restaurant(name);
+        return r;
+    }
 }
