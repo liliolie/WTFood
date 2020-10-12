275b28681c31748b4940660a27db682cbef58965
Junliang Liu <u7096757@anu.edu.au>
2020-10-13 00:48:00 +0800

    update the data transfer.

--- app/src/main/java/com/example/wtfood/MainActivity.java
+++ app/src/main/java/com/example/wtfood/MainActivity.java
@@ -5,22 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.ListView;
 
 import com.example.wtfood.fileprocess.FileProcess;
 import com.example.wtfood.fileprocess.Restaurant;
 import com.example.wtfood.fileprocess.Type;
-import com.example.wtfood.rbtree.Node;
 import com.example.wtfood.rbtree.RBTree;
+import com.google.gson.Gson;
 
 import java.io.IOException;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
     private RBTree priceTree;
     private RBTree raringTree;
-    private ArrayList<String> restaurant_info;
+    private List<Restaurant> restaurants;
 
 
 
@@ -54,18 +48,14 @@ public class MainActivity extends AppCompatActivity {
         }
 
         // Here can add our data in the array list.
-        restaurant_info = new ArrayList<>();
-        ArrayList<Restaurant> restaurants = new ArrayList<>();
-        restaurants.add(createRestaurant("Hero burger"));
-        restaurants.add(createRestaurant("KFC"));
-        restaurants.add(createRestaurant("Subway"));
-        restaurants.add(createRestaurant("Tacobell"));
-        restaurants.add(createRestaurant("BurgerKing"));
-        restaurants.add(createRestaurant("Raku"));
+        restaurants = new ArrayList<>();
+        restaurants.add(new Restaurant("Hero burger"));
+        restaurants.add(new Restaurant("KFC"));
+        restaurants.add(new Restaurant("Subway"));
+        restaurants.add(new Restaurant("Tacobell"));
+        restaurants.add(new Restaurant("BurgerKing"));
+        restaurants.add(new Restaurant("Raku"));
 
-        for (int i = 0; i < restaurants.size(); i++) {
-            restaurant_info.add(display(restaurants.get(i)));
-        }
         ImageButton go = (ImageButton) findViewById(R.id.goButton);
         go.setOnClickListener(l1);
     }
@@ -84,41 +74,11 @@ public class MainActivity extends AppCompatActivity {
     private View.OnClickListener l1 = new View.OnClickListener() {
         public void onClick(View v) {
             // Passing the array to List_Activity.
-            Intent intent = new Intent(getApplicationContext(), List_Activity.class);
-            intent.putExtra("R", restaurant_info);
+            Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+            System.out.println(restaurants.size());
+            intent.putExtra("Restaurants", new Gson().toJson(restaurants));
             startActivity(intent);
         }
     };
 
-
-    public String display(Restaurant r) {
-        String Address = r.getAddress();
-        String name = r.getName();
-        int rating = r.getRating();
-        int price = r.getPrice();
-        String phone = r.getPhone();
-        String delivery = "";
-        String type = "";
-        if (r.isDeliveryService()) {
-            delivery = "Deliverable.";
-        } else {
-            delivery = "Unable to for delivery.";
-        }
-
-        if (r.getType().equals(Type.chinesefood)) {
-            type = "Chinese Food!";
-        }
-        if (r.getType().equals(Type.fastfood)) {
-            type = "Fast Food!";
-        }
-
-        return name + " \n " + Address + " | " + phone + " \n " + "Rating: " + Integer.toString(rating) + " \n " + "Price: " +
-                Integer.toString(price) + " \n " + delivery + " \n " + "FoodType: " + type;
-
-    }
-
-    private Restaurant createRestaurant(String name) {
-        Restaurant r = new Restaurant(name);
-        return r;
-    }
 }
--- app/src/main/java/com/example/wtfood/ResultActivity.java
+++ app/src/main/java/com/example/wtfood/ResultActivity.java
@@ -2,38 +2,36 @@ package com.example.wtfood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import com.example.wtfood.fileprocess.Location;
 import com.example.wtfood.fileprocess.Restaurant;
 import com.example.wtfood.fileprocess.Type;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
-import java.sql.SQLOutput;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.List;
 
-public class List_Activity extends AppCompatActivity {
+public class ResultActivity extends AppCompatActivity {
     ListView result;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_);
+        setContentView(R.layout.activity_result);
 
         result = (ListView) findViewById(R.id.result_lv);
-        ArrayList<String> R = getIntent().getStringArrayListExtra("R");
+        String bookJson =  getIntent().getStringExtra("Restaurants");
+        List<Restaurant> restaurants = new Gson().fromJson(bookJson, new TypeToken<List<Restaurant>>() {}.getType());
+        System.out.println(restaurants.size());
 
 
-        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_list_item_1, R);
+        ArrayAdapter aa = new ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants);
         result.setAdapter(aa);
 
     }
-
-
 
 }
