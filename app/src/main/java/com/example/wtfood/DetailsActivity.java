d4271e537f7b4a7923245a86eb7d85e82b129bd9
Junliang Liu <u7096757@anu.edu.au>
2020-10-22 19:17:10 +0800

    fix a small bug in getExtra.

--- app/src/main/java/com/example/wtfood/DetailsActivity.java
+++ app/src/main/java/com/example/wtfood/DetailsActivity.java
@@ -23,8 +23,9 @@ public class DetailsActivity extends AppCompatActivity {
 
         // get the information and show [Lili]
         Intent intent = getIntent();
-        String bookJson = getIntent().getStringExtra("Restaurants");
-        Restaurant restaurant = new Gson().fromJson(bookJson, Restaurant.class);
+        String restaurantJSON = getIntent().getStringExtra("Restaurant");
+        Restaurant restaurant = new Gson().fromJson(restaurantJSON, Restaurant.class);
+        System.out.println(restaurant);
 
 
         // set the ImageView
